c0080c09db17584fac63361a8f36af51fa3ab426
Bishal Poudel <poudelb172@gmail.com>
2022-04-20 09:23:55 +0545

    update consul done !!!

--- Backend/controllers/consulController.js
+++ Backend/controllers/consulController.js
@@ -19,5 +19,24 @@ exports.getAllConsuls = async (req, res) => {
 }
 
 exports.updateConsul = async (req, res) => {
+    let consul = await Consul.findById(req.params.id)
+
+    if (!consul) {
+        return res.status(500).json({
+            success: false,
+            message: "Not Found"
+        })
+    }
+
+    consul = await Consul.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runvalidatos: true,
+        useFindAndModify: false
+    })
+
+    res.status(200).json({
+        success: true,
+        consul
+    })
 
 }
